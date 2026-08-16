1ebb9af32908e45f8392c305b8e34866d0faae6e
jiwon0995 <jiwon0995@gmail.com>
2026-08-16 13:08:53 +0900

    feat(recommend): 취향 벡터 · 스타일 우선 추천 엔진 구현

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,14 @@
 export { cn } from "./cn";
 export {
-  buildReasons,
-  buildTargetProfile,
-  collectTags,
+  buildReason,
+  buildUserVector,
   getSelectedOptions,
+  pickAnother,
   recommend,
   scoreBeer,
+} from "./recommend";
+export type {
+  PickAnotherOptions,
+  Random,
+  RecommendOptions,
 } from "./recommend";

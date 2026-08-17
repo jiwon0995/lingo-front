acaeef1a053ab7bbeec17c3f3f1cf4e18f05db43
jiwon0995 <jiwon0995@gmail.com>
2026-08-17 12:46:15 +0900

    feat(result): 결과 카드 화면 · 이미지 폴백 구현

---
+++ src/components/screens/result/index.ts
@@ -0,0 +1,4 @@
+export { BeerPhoto } from "./BeerPhoto";
+export type { BeerPhotoProps } from "./BeerPhoto";
+export { ResultScreen } from "./ResultScreen";
+export type { ResultScreenProps } from "./ResultScreen";
